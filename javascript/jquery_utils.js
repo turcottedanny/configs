f5e91cb50aeac01e1017f70d0ba27617bf98e557
Danny Turcotte <danny@dannyturcotte.com>
2012-02-27 20:55:52 -0500

    Ajout de la méthode nearest pour jQuery

---
+++ javascript/jquery_utils.js
@@ -0,0 +1,25 @@
+/* Permet de retrouver le plus près élément en remontant le dom */
+
+(function($) {
+  /*
+   * Retrieve the nearest selector
+   * 
+   * @author Danny Turcotte <turcottedanny@gmail.com>
+   */
+
+  $.fn.nearest = function (selector){
+        if($(this).find(selector).length){
+          
+          return $(this).find(selector);
+          
+        }else if($(this).parent()){
+          
+          return $(this).parent().nearest(selector);
+          
+        }else{
+          
+          return false;
+          
+        }
+  }
+})(jQuery);
